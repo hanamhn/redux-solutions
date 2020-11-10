eab224a396dad11b6b3dde022ee96d41872fde5c
hanamhn <hanamhndev@gmail.com>
2020-11-10 16:11:58 +0700

    initial redux

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { selectSong } from "../actions";
+
+class App extends Component {
+  render() {
+    return <div>App</div>;
+  }
+}
+
+export default App;
